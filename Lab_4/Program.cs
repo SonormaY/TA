c6b6682b1f37f5ef26cd78b32577788eed6562f7
Stanislav Humeniuk <stanislav.humeniuk@outlook.com>
2024-04-03 13:40:37 +0300

    anotherfix

---
+++ Lab_4/Program.cs
@@ -0,0 +1,150 @@
+﻿string Execute(Dictionary<string, string> rules, string start, int max = 100, bool show = false)
+{
+    string result = start;
+    int iterations = 0;
+
+    while (iterations < max)
+    {
+        bool substitutionMade = false;
+
+        foreach (var rule in rules)
+        {
+            if (result.Contains(rule.Key))
+            {
+                result = result.Replace(rule.Key, rule.Value);
+                substitutionMade = true;
+                break;
+            }
+        }
+        if (show)
+        {
+            Console.WriteLine(result);
+        }
+
+        if (!substitutionMade)
+        {
+            break;
+        }
+
+        iterations++;
+    }
+
+    return result;
+}
+
+void Task1()
+{
+    Dictionary<string, string> rules = new Dictionary<string, string>
+    {
+        { "yx", "xy" },
+        { "xy", "" }
+    };
+    string ex1 = "yxxyyxyxyxyxyxxxyxyxyyyx";
+    string ex2 = "xxxyxyxxxyxy";
+    Console.WriteLine("Task 1");
+    Console.WriteLine($"Example ({ex1}) -> {Execute(rules, ex1)}");
+    Console.WriteLine($"Example ({ex2}) -> {Execute(rules, ex2)}\n");
+}
+
+void Task2()
+{
+    Dictionary<string, string> rules = new Dictionary<string, string>
+    {
+        { "yyx", "y" },
+        { "xx", "y" },
+        { "yyy", "x" }
+    };
+    string ex1 = "yyyyyyyyyyyy";
+    string ex2 = "yyxyxyyxyxyxxxyxyxxyxyxy";
+    Console.WriteLine("Task 2");
+    Console.WriteLine($"Example ({ex1}) -> {Execute(rules, ex1, 1000)}");
+    Console.WriteLine($"Example ({ex2}) -> {Execute(rules, ex2, 1000)}\n");
+}
+
+void Task3()
+{
+    Dictionary<string, string> rules = new Dictionary<string, string>
+    {
+        { "1-1", "-0"},
+        { "01", "1"},
+        { "-0", ""}
+    };
+    string ex1 = "1111-111";
+    string ex2 = "111-11111";
+    Console.WriteLine("Task 3");
+    Console.WriteLine($"Example ({ex1}) -> {Execute(rules, ex1)}");
+    Console.WriteLine($"Example ({ex2}) -> {Execute(rules, ex2)}\n");
+}
+
+void Task4()
+{
+    Dictionary<string, string> rules = new Dictionary<string, string>
+    {
+        { "1+1", "11+0"},
+        { "01", "1"},
+        { "+0", ""}
+    };
+    string ex1 = "1111+111";
+    string ex2 = "111+11111";
+    Console.WriteLine("Task 4");
+    Console.WriteLine($"Example ({ex1}) -> {Execute(rules, ex1)}");
+    Console.WriteLine($"Example ({ex2}) -> {Execute(rules, ex2)}\n");
+}
+
+void Task5()
+{
+    Dictionary<string, string> rules = new Dictionary<string, string>
+    {
+        { "*11", "T*1"},
+        { "*1", "T"},
+        { "1T", "T1F"},
+        { "FT", "TF"},
+        { "F1", "1F"},
+        { "T1", "T"},
+        { "TF", "F"},
+        { "F", "1"}
+    };
+    string ex1 = "111*1111";
+    Console.WriteLine("Task 5");
+    Console.WriteLine($"Example ({ex1}) -> {Execute(rules, ex1, show: true)}\n");
+}
+
+void Task6()
+{
+    //Скласти нормальний алгоритм, який видаляє всі входження, крім першого, букви а з рядка символів, побудованого в алфавіті А={a, b, c}.
+    Dictionary<string, string> rules = new Dictionary<string, string>
+    {
+        { "a", "T"}
+    };
+    string ex1 = "acaabaa";
+    string ex2 = "bacabac";
+    Console.WriteLine("Task 6");
+    Console.WriteLine($"Example ({ex1}) -> {Execute(rules, ex1)}");
+    Console.WriteLine($"Example ({ex2}) -> {Execute(rules, ex2)}\n");
+}
+
+void Task7()
+{
+    Dictionary<string, string> rules = new Dictionary<string, string>
+    {
+        { "  ", " "}
+    };
+    string ex1 = "a  a b  a  b c  c ";
+    string ex2 = "ab a ab c c  c  b";
+    string ex3 = "a  b  c  a  b  c";
+    Console.WriteLine("Task 7");
+    Console.WriteLine($"Example ({ex1}) -> {Execute(rules, ex1)}");
+    Console.WriteLine($"Example ({ex2}) -> {Execute(rules, ex2)}");
+    Console.WriteLine($"Example ({ex3}) -> {Execute(rules, ex3)}\n");
+}
+
+
+
+Console.Clear();
+Task1();
+Task2();
+Task3();
+Task4();
+Task5();
+Task6();
+Task7();
---
+++ Lab_6/TuringMachine.cs
@@ -0,0 +1,215 @@
+namespace Lab_6;
+
+class TuringMachine
+{
+    public enum State
+    {
+        q0, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, qFin
+    }
+    public enum Direction
+    {
+        L, R
+    }
+    private readonly Dictionary<State, Dictionary<char, Tuple<char, Direction, State>>> rulesUnaryAddition = new Dictionary<State, Dictionary<char, Tuple<char, Direction, State>>> {
+        { State.q0, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'I', Tuple.Create('X', Direction.R, State.q1) },
+            { ' ', Tuple.Create(' ', Direction.R, State.qFin) }
+        }},
+        { State.q1, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'I', Tuple.Create('I', Direction.R, State.q1) },
+            { ' ', Tuple.Create(' ', Direction.R, State.q2) }
+        }},
+        { State.q2, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'I', Tuple.Create('I', Direction.R, State.q2) },
+            { ' ', Tuple.Create('I', Direction.L, State.q3) }
+        }},
+        { State.q3, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'I', Tuple.Create('I', Direction.L, State.q3) },
+            { ' ', Tuple.Create(' ', Direction.L, State.q4) }
+        }},
+        { State.q4, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'I', Tuple.Create('I', Direction.L, State.q4) },
+            { 'X', Tuple.Create('X', Direction.R, State.q0) }
+        }}
+    };
+    public readonly Dictionary<State, Dictionary<char, Tuple<char, Direction, State>>> rulesBinaryAddition = new Dictionary<State, Dictionary<char, Tuple<char, Direction, State>>> {
+        { State.q0, new Dictionary<char, Tuple<char, Direction, State>> {
+            { '0', Tuple.Create('0', Direction.R, State.q0)},
+            { '1', Tuple.Create('1', Direction.R, State.q0)},
+            { ' ', Tuple.Create( ' ', Direction.R, State.q1)}
+        }},
+        { State.q1, new Dictionary<char, Tuple<char, Direction, State>> {
+            { '0', Tuple.Create('0', Direction.R, State.q1)},
+            { '1', Tuple.Create('1', Direction.R, State.q1)},
+            { ' ', Tuple.Create(' ', Direction.L, State.q2)}
+        }},
+        { State.q2, new Dictionary<char, Tuple<char, Direction, State>> {
+            { '0', Tuple.Create('1', Direction.L, State.q2)},
+            { '1', Tuple.Create('0', Direction.L, State.q3)},
+            { ' ', Tuple.Create(' ', Direction.R, State.q5)}
+        }},
+        { State.q3, new Dictionary<char, Tuple<char, Direction, State>> {
+            { '0', Tuple.Create('0', Direction.L, State.q3)},
+            { '1', Tuple.Create('1', Direction.L, State.q3)},
+            { ' ', Tuple.Create(' ', Direction.L, State.q4)}
+        }},
+        { State.q4, new Dictionary<char, Tuple<char, Direction, State>> {
+            { '0', Tuple.Create('1', Direction.R, State.q0)},
+            { '1', Tuple.Create('0', Direction.L, State.q4)},
+            { ' ', Tuple.Create('1', Direction.R, State.q0)}
+        }},
+        { State.q5, new Dictionary<char, Tuple<char, Direction, State>> {
+            { '1', Tuple.Create(' ', Direction.R, State.q5)},
+            { ' ', Tuple.Create(' ', Direction.R, State.qFin)}
+        }}
+    };
+    public readonly Dictionary<State, Dictionary<char, Tuple<char, Direction, State>>> rulesUnarySubstraction = new Dictionary<State, Dictionary<char, Tuple<char, Direction, State>>> {
+        { State.q0, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'I', Tuple.Create('X', Direction.R, State.q1)},
+            { ' ', Tuple.Create(' ', Direction.R, State.q5)}
+        }},
+        { State.q1, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'I', Tuple.Create('I', Direction.R, State.q1)},
+            { ' ', Tuple.Create(' ', Direction.R, State.q2)}
+        }},
+        { State.q2, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'X', Tuple.Create('X', Direction.R, State.q2)},
+            { 'I', Tuple.Create('X', Direction.L, State.q3)}
+        }},
+        { State.q3, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'X', Tuple.Create('X', Direction.L, State.q3)},
+            { ' ', Tuple.Create(' ', Direction.L, State.q4)}
+        }},
+        { State.q4, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'I', Tuple.Create('I', Direction.L, State.q4)},
+            { 'X', Tuple.Create('X', Direction.R, State.q0)}
+        }},
+        { State.q5, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'X', Tuple.Create('X', Direction.R, State.q5)},
+            { 'I', Tuple.Create('I', Direction.L, State.q6)}
+        }},
+        { State.q6, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'X', Tuple.Create(' ', Direction.R, State.qFin)}
+        }}
+    };
+    public readonly Dictionary<State, Dictionary<char, Tuple<char, Direction, State>>> rulesUnaryMultiplication = new Dictionary<State, Dictionary<char, Tuple<char, Direction, State>>> {
+        { State.q0, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'I', Tuple.Create('I', Direction.R, State.q0)},
+            { ' ', Tuple.Create(' ', Direction.R, State.q1)}
+        }},
+        { State.q1, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'I', Tuple.Create('I', Direction.R, State.q1)},
+            { ' ', Tuple.Create(' ', Direction.L, State.q2)}
+        }},
+        { State.q2, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'I', Tuple.Create('I', Direction.L, State.q2)},
+            { ' ', Tuple.Create(' ', Direction.R, State.q3)}
+        }},
+        { State.q3, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'X', Tuple.Create('X', Direction.R, State.q3)},
+            { 'I', Tuple.Create('X', Direction.L, State.q4)},
+            { ' ', Tuple.Create(' ', Direction.L, State.q12)}
+        }},
+        { State.q4, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'X', Tuple.Create('X', Direction.L, State.q4)},
+            { ' ', Tuple.Create(' ', Direction.L, State.q5)}
+        }},
+        { State.q5, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'Y', Tuple.Create('Y', Direction.L, State.q5)},
+            { 'I', Tuple.Create('Y', Direction.R, State.q6)},
+            { ' ', Tuple.Create(' ', Direction.R, State.q11)}
+        }},
+        { State.q6, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'Y', Tuple.Create('Y', Direction.R, State.q6)},
+            { ' ', Tuple.Create(' ', Direction.R, State.q7)}
+        }},
+        { State.q7, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'I', Tuple.Create('I', Direction.R, State.q7)},
+            { 'X', Tuple.Create('X', Direction.R, State.q7)},
+            { ' ', Tuple.Create(' ', Direction.R, State.q8)}
+        }},
+        { State.q8, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'I', Tuple.Create('I', Direction.R, State.q8)},
+            { ' ', Tuple.Create('I', Direction.L, State.q9)}
+        }},
+        { State.q9, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'I', Tuple.Create('I', Direction.L, State.q9)},
+            { ' ', Tuple.Create(' ', Direction.L, State.q10)}
+        }},
+        { State.q10, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'I', Tuple.Create('I', Direction.L, State.q10)},
+            { 'X', Tuple.Create('X', Direction.L, State.q10)},
+            { ' ', Tuple.Create(' ', Direction.L, State.q5)}
+        }},
+        { State.q11, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'Y', Tuple.Create('I', Direction.R, State.q11)},
+            { ' ', Tuple.Create(' ', Direction.R, State.q3)}
+        }},
+        { State.q12, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'X', Tuple.Create(' ', Direction.L, State.q12)},
+            { ' ', Tuple.Create(' ', Direction.L, State.q12)},
+            { 'I', Tuple.Create(' ', Direction.L, State.q13)}
+        }},
+        { State.q13, new Dictionary<char, Tuple<char, Direction, State>> {
+            { 'I', Tuple.Create(' ', Direction.L, State.q13)},
+            { ' ', Tuple.Create(' ', Direction.R, State.qFin)}
+        }}
+    };
+    public readonly Dictionary<State, Dictionary<char, Tuple<char, Direction, State>>> rulesConverter = new Dictionary<State, Dictionary<char, Tuple<char, Direction, State>>> {
+        
+    };
+    static bool ValidateString(string first, string second)
+    {
+        return first.All(c => c == 'I') && second.All(c => c == 'I') ||
+               first.All(c => c == '0' || c == '1') && second.All(c => c == '0' || c == '1');
+    }
+    static string PrepareString(string first, string second)
+    {
+        if (!ValidateString(first, second))
+        {
+            throw new ArgumentException("Invalid input strings");
+        }
+        return new string(' ', first.Length + second.Length) + first + ' ' + second + new string(' ', (first.Length + second.Length * 2));
+    }
+    string FinalizeString(string tape)
+    {
+        return tape.Trim('X', ' ');
+    }
+    public Dictionary<State, Dictionary<char, Tuple<char, Direction, State>>> GetRules(int Operation = 0)
+    {
+        switch (Operation)
+        {
+            case 0:
+                return rulesUnaryAddition;
+            case 1:
+                return rulesBinaryAddition;
+            case 2:
+                return rulesUnarySubstraction;
+            case 3:
+                return rulesUnaryMultiplication;
+            case 4:
+                return rulesConverter;
+            default:
+                throw new ArgumentException("Invalid operation");
+        }
+    }
+    public string TuringMachineRun(string first, string second = "", int Operation = 0,  bool debug = false)
+    {
+        Dictionary<State, Dictionary<char, Tuple<char, Direction, State>>> rules = GetRules(Operation);
+        char[] tape = PrepareString(first, second).ToCharArray();
+        State currentState = State.q0;
+        int i = Array.FindIndex(tape, c => c != ' ');
+        while (currentState != State.qFin)
+        {
+            char currentSymbol = tape[i];
+            tape[i] = rules[currentState][currentSymbol].Item1;
+            i = rules[currentState][currentSymbol].Item2 == Direction.R ? i + 1 : i - 1;
+            currentState = rules[currentState][currentSymbol].Item3;
+            if (debug)
+            {
+                Console.WriteLine(new string(tape) + " " + currentState + " " + i);
+            }
+        }
+        return debug ? new string(tape) : FinalizeString(new string(tape));
+    }
+}
